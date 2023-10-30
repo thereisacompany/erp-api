7fafd8e0526034359adf303957f5298e0a776a4b
james <bbjames611@gmail.com>
2023-10-30 20:04:59 +0800

    fixed: 1. 查詢單子詳細資料，命名未改。

--- src/main/java/com/jsh/erp/datasource/mappers/DepotHeadMapperEx.java
+++ src/main/java/com/jsh/erp/datasource/mappers/DepotHeadMapperEx.java
@@ -192,7 +192,7 @@ public interface DepotHeadMapperEx {
             @Param("endTime") String endTime);
 
     List<DepotHeadVo4List> getDetailByNumber(
-            @Param("number") String[] numbers);
+            @Param("numbers") String[] numbers);
 
     int batchDeleteDepotHeadByIds(@Param("updateTime") Date updateTime, @Param("updater") Long updater, @Param("ids") String ids[]);
 
