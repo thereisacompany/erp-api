110678cc9b2316f1102a9d6676a02a842a076959
chien-chih <chien-chih@mail.chungyo.net>
2023-12-05 17:34:17 +0800

    fixed: 1. 匯出的檔案名稱，以utf8格式設定。

--- src/main/java/com/jsh/erp/utils/ExportExecUtil.java
+++ src/main/java/com/jsh/erp/utils/ExportExecUtil.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -12,21 +14,22 @@ import java.util.zip.ZipOutputStream;
 public class ExportExecUtil {
 
 	public static void showExec(File excelFile, String fileName, HttpServletResponse response) throws Exception{
-		   response.setContentType("application/octet-stream");
-	       fileName = new String(fileName.getBytes("gbk"),"ISO8859_1");
-		   response.setHeader("Content-Disposition", "attachment;filename=\"" + fileName + ".xls" + "\"");
-		   FileInputStream fis = new FileInputStream(excelFile);
-		   OutputStream out = response.getOutputStream();
+		response.setContentType("application/octet-stream");
+//	       fileName = new String(fileName.getBytes("gbk"),"ISO8859_1");
+		fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8.toString());
+		response.setHeader("Content-Disposition", "attachment;filename=\"" + fileName + "\""); //".xls" +
+		FileInputStream fis = new FileInputStream(excelFile);
+		OutputStream out = response.getOutputStream();
+		int SIZE = 1024 * 1024;
+		byte[] bytes = new byte[SIZE];
+		int LENGTH = -1;
+		while((LENGTH = fis.read(bytes)) != -1){
+			out.write(bytes,0,LENGTH);
+		}
 
-		   int SIZE = 1024 * 1024;
-		   byte[] bytes = new byte[SIZE];
-		   int LENGTH = -1;
-		   while((LENGTH = fis.read(bytes)) != -1){
-			   out.write(bytes,0,LENGTH);
-		   }
+		out.flush();
+		fis.close();
 
-		   out.flush();
-		   fis.close();
 	}
 
 	public static void showExecs(List<File> list, HttpServletResponse response) throws IOException {
