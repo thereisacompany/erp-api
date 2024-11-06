552e02169f51695dff0d3b8d721002860f56f8e7
james <>
2024-11-06 21:34:21 +0800

    fixed: 1. 移除取得司機列表時，前面串上的id。 2. 司機配送統計表-依司機名稱查詢，會查無資料。

--- src/main/java/com/jsh/erp/service/supplier/SupplierService.java
+++ src/main/java/com/jsh/erp/service/supplier/SupplierService.java
@@ -165,10 +165,10 @@ public class SupplierService {
                     sum = sum.add(beginNeedPay);
                     s.setAllNeedPay(sum);
                 } else if(subType.contains("司機") || subType.contains("師傅")) {
-                    if(rows >= 1000) {
-                        String showId = String.format("%03d", s.getId());
-                        s.setSupplier(showId + " " + s.getSupplier());
-                    }
+//                    if(rows >= 1000) {
+//                        String showId = String.format("%03d", s.getId());
+//                        s.setSupplier(showId + " " + s.getSupplier());
+//                    }
 
                     s.setLoginName(supplierMapper.selectCarUser(s.getId()));
                     Optional<Vehicle> obj = vList.parallelStream()
